c33070b3494906e21fea72485c6acd2bbe7faa82
memade <k34ubll5@outlook.com>
2023-01-24 16:14:34 +0800

    project configure.

---
+++ src/bridge/stdafx.h
@@ -0,0 +1,14 @@
+﻿#if !defined(__9DC89752_2CFC_4A8A_B8CD_66AC10660EED__)
+#define __9DC89752_2CFC_4A8A_B8CD_66AC10660EED__
+
+#include <win.h>
+#include <spdlog.hpp>
+#include <openssl.hpp>
+#include <iconv.hpp>
+
+
+/// /*_ Memade®（新生™） _**/
+/// /*_ Tue, 24 Jan 2023 04:24:41 GMT _**/
+/// /*_____ https://www.skstu.com/ _____ **/
+#endif///__9DC89752_2CFC_4A8A_B8CD_66AC10660EED__
+
